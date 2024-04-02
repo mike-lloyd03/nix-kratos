c16efc2bbfe5ce4c3941e50077c1e7a65083a0aa
Mike Lloyd <mike.lloyd03@pm.me>
2024-04-02 10:09:27 -0700

    Gen 149

--- configuration.nix
+++ configuration.nix
@@ -125,6 +125,79 @@
           };
         };
       };
+      keychron = {
+        ids = [ "05ac:024f" ];
+        settings = {
+          main = {
+            f1 = "brightnessdown";
+            f2 = "brightnessup";
+            f5 = "kbdillumdown";
+            f6 = "kbdillumup";
+            f7 = "previoussong";
+            f8 = "playpause";
+            f9 = "nextsong";
+            f10 = "mute";
+            f11 = "volumedown";
+            f12 = "volumeup";
+            control = "layer(meta)";
+            alt = "layer(control)";
+            meta = "layer(alt)";
+            capslock = "overload(nav, esc)";
+            "leftshift+rightshift" = "capslock";
+          };
+          control = {
+            f1 = "f1";
+            f2 = "f2";
+            f3 = "f3";
+            f5 = "f5";
+            f6 = "f6";
+            f7 = "f7";
+            f8 = "f8";
+            f9 = "f9";
+            f10 = "f10";
+            f11 = "f11";
+            f12 = "f12";
+          };
+          shift = {
+            f1 = "f1";
+            f2 = "f2";
+            f3 = "f3";
+            f5 = "f5";
+            f6 = "f6";
+            f7 = "f7";
+            f8 = "f8";
+            f9 = "f9";
+            f10 = "f10";
+            f11 = "f11";
+            f12 = "f12";
+          };
+          control-alt = {
+            f1 = "f1";
+            f2 = "f2";
+            f3 = "f3";
+            f5 = "f5";
+            f6 = "f6";
+            f7 = "f7";
+            f8 = "f8";
+            f9 = "f9";
+            f10 = "f10";
+            f11 = "f11";
+            f12 = "f12";
+          };
+          nav = {
+            h = "left";
+            k = "up";
+            j = "down";
+            l = "right";
+            y = "copy";
+            p = "paste";
+            x = "cut";
+            u = "undo";
+            "0" = "home";
+            "4" = "end";
+          };
+        };
+      };
     };
   };
 
