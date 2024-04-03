fce191ef164bf69f5ee211552603748d160287b3
Mike Lloyd <mike.lloyd03@pm.me>
2024-04-02 23:56:14 -0700

    Gen 168

--- configuration.nix
+++ configuration.nix
@@ -240,7 +240,7 @@
     modesetting.enable = true;
     #   # Fine-grained power management. Turns off GPU when not in use.
     #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+    powerManagement.finegrained = true;
     open = false;
     nvidiaSettings = true;
 
