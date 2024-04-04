6acd7bcf4c8c96471c405bc0d6ae0d26c10ef73e
Mike Lloyd <mike.lloyd03@pm.me>
2024-04-03 22:49:06 -0700

    Gen 175

---
+++ keyd/internal.nix
@@ -0,0 +1,73 @@
+{
+  ids = [ "1532:0253" ];
+  settings = {
+    main = {
+      f1 = "mute";
+      f2 = "volumedown";
+      f3 = "volumeup";
+      f5 = "previoussong";
+      f6 = "playpause";
+      f7 = "nextsong";
+      f8 = "brightnessdown";
+      f9 = "brightnessup";
+      f10 = "kbdillumdown";
+      f11 = "kbdillumup";
+      control = "layer(meta)";
+      alt = "layer(control)";
+      meta = "layer(alt)";
+      capslock = "overload(nav, esc)";
+      "leftshift+rightshift" = "capslock";
+    };
+    control = {
+      f1 = "f1";
+      f2 = "f2";
+      f3 = "f3";
+      f5 = "f5";
+      f6 = "f6";
+      f7 = "f7";
+      f8 = "f8";
+      f9 = "f9";
+      f10 = "f10";
+      f11 = "f11";
+      f12 = "f12";
+    };
+    shift = {
+      f1 = "f1";
+      f2 = "f2";
+      f3 = "f3";
+      f5 = "f5";
+      f6 = "f6";
+      f7 = "f7";
+      f8 = "f8";
+      f9 = "f9";
+      f10 = "f10";
+      f11 = "f11";
+      f12 = "f12";
+    };
+    control-alt = {
+      f1 = "f1";
+      f2 = "f2";
+      f3 = "f3";
+      f5 = "f5";
+      f6 = "f6";
+      f7 = "f7";
+      f8 = "f8";
+      f9 = "f9";
+      f10 = "f10";
+      f11 = "f11";
+      f12 = "f12";
+    };
+    nav = {
+      h = "left";
+      k = "up";
+      j = "down";
+      l = "right";
+      y = "copy";
+      p = "paste";
+      x = "cut";
+      u = "undo";
+      "0" = "home";
+      "4" = "end";
+    };
+  };
+}
