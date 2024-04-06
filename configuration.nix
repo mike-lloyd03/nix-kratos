221e11d870f7c74c8bf06e91cb5e03c74e474dc5
Mike Lloyd <mike.lloyd03@pm.me>
2024-04-06 00:19:15 -0700

    Gen 202

--- configuration.nix
+++ configuration.nix
@@ -23,8 +23,8 @@
     videoDrivers = [ "nvidia" ];
     # desktopManager.xfce.enable = true;
     desktopManager.gnome.enable = true;
-    displayManager.startx.enable = true;
-    # displayManager.gdm.enable = true;
+    # displayManager.startx.enable = true;
+    displayManager.gdm.enable = true;
     # displayManager.sddm = {
     #   enable = true;
     #   theme = "Elegant";
